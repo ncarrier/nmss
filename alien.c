d7e95313acbbd056c74e333c0d09206f5218b68a
Nicolas Carrier <carrier.nicolas0@gmail.com>
2019-05-19 17:04:01 +0200

    one different alien design for each wave

--- alien.c
+++ alien.c
@@ -1,7 +1,32 @@
 #include "alien.h"
 #include "screen.h"
+#include "utils.h"
 
-#include "res/alien.xpm"
+#include "res/alien_0.xpm"
+#include "res/alien_1.xpm"
+#include "res/alien_2.xpm"
+#include "res/alien_3.xpm"
+#include "res/alien_4.xpm"
+#include "res/alien_5.xpm"
+#include "res/alien_6.xpm"
+#include "res/alien_7.xpm"
+#include "res/alien_8.xpm"
+#include "res/alien_9.xpm"
+
+typedef char *alien_image[];
+
+static alien_image *alien_images[] = {
+		&alien_0_xpm,
+		&alien_1_xpm,
+		&alien_2_xpm,
+		&alien_3_xpm,
+		&alien_4_xpm,
+		&alien_5_xpm,
+		&alien_6_xpm,
+		&alien_7_xpm,
+		&alien_8_xpm,
+		&alien_9_xpm,
+};
 
 #define ALIEN_FLIP_PERIOD 20
 
@@ -14,7 +39,8 @@ static const struct SDL_Rect alien_init_pos = {
 
 void alien_init(struct alien *alien, struct SDL_Renderer *renderer,
 		int movement, unsigned alien_index) {
-	object_init(&alien->object, renderer, &alien_init_pos, alien_xpm);
+	object_init(&alien->object, renderer, &alien_init_pos,
+			*(alien_images[movement % ARRAY_SIZE(alien_images)]));
 	alien->dead = false;
 	alien->flip_counter = ALIEN_FLIP_PERIOD;
 	alien_movement_init(&alien->movement, movement, alien_index);
